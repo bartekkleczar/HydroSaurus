dd4a93cd0533f941688a8af91c7c5eb367629557
Bartosz Klęczar <128930025+bartekkleczar@users.noreply.github.com>
2024-11-11 17:07:57 +0100

    1st. ver. homeUI

--- app/src/main/java/com/example/hydrosaurus/ui/theme/Color.kt
+++
@@ -1,9 +0,0 @@
-package com.example.hydrosaurus.ui.theme
-
-import androidx.compose.ui.graphics.Color
-
-val WhiteMain = Color(0xFFAAAAAA)
-val BlackMain = Color(0xFF0E1E0E)
-val BlueSecond = Color(0xFF0e3b68)
-val BlueThird = Color(0xFF3B96A7)
-val BlueFourth = Color(0xFF8fcfce)
